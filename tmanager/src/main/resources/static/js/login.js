94d71fa19c9b2105c76573d53ec685aa6f8870be
lan <1109373967@qq.com>
2021-05-15 21:04:48 +0800

    大体结构完善

--- tmanager/src/main/resources/static/js/login.js
+++ tmanager/src/main/resources/static/js/login.js
@@ -1,3 +1,13 @@
 /**
- * 
+ *   登陆页面脚本
  */
+ /* id */
+function idChange(id){
+	if(id=='1'){
+		$('#stu').attr('class','active');
+		$('#tea').attr('class','');
+	}else{
+		$('#stu').attr('class','');
+		$('#tea').attr('class','active');
+	}
+}
