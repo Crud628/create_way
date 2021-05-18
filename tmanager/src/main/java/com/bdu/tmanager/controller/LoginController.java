833c8b895752538b78544ae12b72e729a7f2f60e
lan <1109373967@qq.com>
2021-05-18 22:50:20 +0800

    登录功能和日志补充（待完善）

--- tmanager/src/main/java/com/bdu/tmanager/controller/LoginController.java
+++ tmanager/src/main/java/com/bdu/tmanager/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.bdu.tmanager.controller;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,11 +10,12 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
+import com.bdu.tmanager.bean.Rzgl;
 import com.bdu.tmanager.bean.Student;
 import com.bdu.tmanager.bean.Teacher;
 import com.bdu.tmanager.controller.base.BaseController;
+import com.bdu.tmanager.service.RzglService;
 import com.bdu.tmanager.service.StudentService;
 import com.bdu.tmanager.service.TeacherService;
 
@@ -31,6 +33,19 @@ public class LoginController extends BaseController{
 	private TeacherService tService;
 	@Autowired
 	private StudentService sService;
+	@Autowired
+	private RzglService rService;
+	
+	/**
+	 * 已经登录的用户
+	 */
+	public static Map<String,Object> users = new HashMap<String,Object>();
+	
+	/**
+	 * 日志临时记录
+	 */
+	public static Map<String,Object> logs = new HashMap<String,Object>();
+	
 	/**
 	 * 登陆页面路由
 	 * @return
@@ -57,10 +72,25 @@ public class LoginController extends BaseController{
 			data.put("errMsg", "用户名不存在");
 			return data;
 		}
+		Object object = users.get(s.getId());
+		if(object != null) {
+			data.put("success", false);
+			data.put("errMsg", "该用户已经登陆");
+		}
 		if(s.getPassword().equals(student.getPassword())) {
-			data.put("success",true);
-			session.setAttribute("login", s.getId());
+			users.put(s.getId(), s);
+			session.setAttribute("login", users);
 			//存日志
+			Rzgl r = new Rzgl();
+			r.setRid(s.getId());
+			r.setRname(s.getName());
+			r.setIntime(new Date());
+			rService.add(r);
+			logs.put(r.getRno()+"", r);
+			
+			data.put("success",true);
+			data.put("user", s.getId());
+			data.put("log", r.getRno());
 		}else {
 			data.put("success", false);
 			data.put("errMsg", "密码不正确");
@@ -86,9 +116,15 @@ public class LoginController extends BaseController{
 			return data;
 		}
 		if(t.getPassword().equals(teacher.getPassword())) {
-			data.put("success",true);
+			
 			session.setAttribute("login", t.getId());
 			//存日志
+			Rzgl r = new Rzgl();
+			r.setRid(t.getId());
+			r.setRname(t.getName());
+			r.setIntime(new Date());
+			rService.add(r);
+			logs.put(r.getRno()+"", r);
 		}else {
 			data.put("success", false);
 			data.put("errMsg", "密码不正确");
@@ -101,16 +137,16 @@ public class LoginController extends BaseController{
 	 * @return
 	 */
 	@RequestMapping(value="logout")
-	public Map<String,Object> logout(){
+	public Map<String,Object> logout(String id,String logId){
 		Map<String,Object> data = new HashMap<String,Object>();
-		String flag = String.valueOf(session.getAttribute("login"));
-		if(flag != null) {
-			session.removeAttribute("login");
-			//存日志
-			data.put("success", true);
-			return data;
-		}
-		data.put("success", false);
+		
+		String flag=null;
+		//更新登出记录
+		Rzgl r = new Rzgl();
+		r.setRno(Integer.parseInt(logId));
+		r.setOuttime(new Date());
+		rService.update(r);
+		data.put("success", true);
 		return data;
 	}
 	
