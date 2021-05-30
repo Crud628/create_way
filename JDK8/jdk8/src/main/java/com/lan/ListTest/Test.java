f297a9c7f74cd970d4e15dad58b1ece8fd00e4ff
lan <1109373967@qq.com>
2021-05-30 16:19:19 +0800

    Update Test.java

--- JDK8/jdk8/src/main/java/com/lan/ListTest/Test.java
+++ JDK8/jdk8/src/main/java/com/lan/ListTest/Test.java
@@ -1,9 +1,5 @@
 package com.lan.ListTest;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Consumer;
-
 /**
  * 
  * @author lan
@@ -12,35 +8,33 @@ import java.util.function.Consumer;
  */
 public class Test {
 	public static void main(String[] args) {
-		List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8);
-		
-		//原始
-		for(int i = 0; i < list.size(); i++) {
-			System.out.println(list.get(i));
-		}
-		
-		System.out.println("-----------------");
-		
-		//JDK 1.5 增强for循环
-		for (Integer integer : list) {
-			System.out.println(integer);
-		}
-		
-		System.out.println("-----------------");
-		
-		//JDK 1.8新增 Consumer<? super T> action
-		list.forEach(new Consumer<Integer>() {
-			@Override
-			public void accept(Integer t) {
-				// TODO Auto-generated method stub
-				System.out.println(t);
-			}
-		});
-		
-		System.out.println("-----------------");
-		
-		int s = Integer.MAX_VALUE;
-		int t = Integer.MIN_VALUE+10;
-		System.out.println((s & t) + ((s ^ t) >> 1));
+//		List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8);
+//		
+//		//原始
+//		for(int i = 0; i < list.size(); i++) {
+//			System.out.println(list.get(i));
+//		}
+//		
+//		System.out.println("-----------------");
+//		
+//		//JDK 1.5 增强for循环
+//		for (Integer integer : list) {
+//			System.out.println(integer);
+//		}
+//		
+//		System.out.println("-----------------");
+//		
+//		//JDK 1.8新增 Consumer<? super T> action
+//		list.forEach(new Consumer<Integer>() {
+//			@Override
+//			public void accept(Integer t) {
+//				// TODO Auto-generated method stub
+//				System.out.println(t);
+//			}
+//		});
+//		
+//		System.out.println("-----------------");
+//		
 	}
+
 }
