99f0c4435b82d0795603d0629aecc147b566b9c1
lan <1109373967@qq.com>
2021-06-08 20:43:22 +0800

    每日计划

---
+++ dailywork/src/main/java/com/lan/dailywork/DailyworkApplication.java
@@ -0,0 +1,13 @@
+package com.lan.dailywork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyworkApplication.class, args);
+    }
+
+}
