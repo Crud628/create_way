a195c4ee9ab7d09525bf8b7161e3f7207aa3bb7a
lan <1109373967@qq.com>
2021-08-07 14:12:09 +0800

    每日一题

---
+++ LeetCode/August2021/d07LC457/fun1.java
@@ -0,0 +1,44 @@
+package d07LC457;
+
+/**
+ *  快慢指针
+ *  
+ * @author lan
+ * 2021年8月7日
+ * TODO
+ */
+public class fun1 {
+	public boolean circularArrayLoop(int[] nums) {
+		int n = nums.length;
+		for (int i = 0; i < n; i++) {
+			if (nums[i] == 0) {
+				continue;
+			}
+			int slow = i, fast = next(nums, i);
+			// 判断非零且方向相同
+			while (nums[slow] * nums[fast] > 0 && nums[slow] * nums[next(nums, fast)] > 0) {
+				if (slow == fast) {
+					if (slow != next(nums, slow)) {
+						return true;
+					} else {
+						break;
+					}
+				}
+				slow = next(nums, slow);
+				fast = next(nums, next(nums, fast));
+			}
+			int add = i;
+			while (nums[add] * nums[next(nums, add)] > 0) {
+				int tmp = add;
+				add = next(nums, add);
+				nums[tmp] = 0;
+			}
+		}
+		return false;
+	}
+
+	public int next(int[] nums, int cur) {
+		int n = nums.length;
+		return ((cur + nums[cur]) % n + n) % n; // 保证返回值在 [0,n) 中
+	}
+}
