aa862e20c347b6f53fa9fa311d03ad24ebd82a67
lan <1109373967@qq.com>
2021-08-11 22:32:30 +0800

    每日一题

---
+++ LeetCode/August2021/d11LC446/fun1.java
@@ -0,0 +1,24 @@
+package d11LC446;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class fun1 {
+    public int numberOfArithmeticSlices(int[] nums) {
+        int ans = 0;
+        int n = nums.length;
+        Map<Long, Integer>[] f = new Map[n];
+        for (int i = 0; i < n; ++i) {
+            f[i] = new HashMap<Long, Integer>();
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                long d = 1L * nums[i] - nums[j];
+                int cnt = f[j].getOrDefault(d, 0);
+                ans += cnt;
+                f[i].put(d, f[i].getOrDefault(d, 0) + cnt + 1);
+            }
+        }
+        return ans;
+    }
+}
