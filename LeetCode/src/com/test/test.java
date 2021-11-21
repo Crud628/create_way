0466fe32d08cbe57d66aa25d67c2ac19fd295cb8
Crud628 <1109373967@qq.com>
2021-11-21 22:36:08 +0800

    Revert "第九周作业"
    This reverts commit 23a6d536b4dce1eb1bb1a7200c87018de08f3e65.

--- LeetCode/src/com/test/test.java
+++ LeetCode/src/com/test/test.java
@@ -16,15 +16,23 @@ public class test{
 
 	public static void main(String[] args) {
 		int[] nums = {1,2,3,4};
-		String string = "hebought";
+		String string = "he bought 2 pencils, 3 erasers, and 1  pencil-sharpener.";
 		long start = new Date().getTime();
 		
-//		System.out.println(new test() );
+		System.out.println(new test().countValidWords(string) );
 
-		System.out.println(string.substring(0,string.length() - 1));
 		System.out.println("执行时间：" + (new Date().getTime() - start));
 	}
 
+    public int countValidWords(String sentence) {
+    	String[] split = sentence.split("\s");
+    	int count = 0;
+    	for (String string:split) {
+    		if (string.endsWith(",") || string.endsWith(".") || string.endsWith("!")) string = string.substring(0,string.length()-1);
+    		if ("/^[a-z]+-*[a-z]+$/g".matches(string) || "/^[a-z]+$/g".matches(string)) count++;
+    	}
+    	return count;
+    }
 	
 	
 
